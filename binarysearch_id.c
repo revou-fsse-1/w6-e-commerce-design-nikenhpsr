e8fa53d1df535a5a7b60b6c36f94b470a304e867
nikenhpsr <niken18003@mail.unpad.ac.id>
2023-03-02 05:43:31 +0700

    add loo for more dynamic

--- binarysearch_id.c
+++ binarysearch_id.c
@@ -14,35 +14,39 @@ int main(void)
         n += pow(2,i);
         n--;
     }
-        printf("There are %li possible IDs\n", n);
-
-    do {
-        product_id = printf("Product ID: ");
-        scanf("%li", &product_id);
+    
+    do 
+    {
+        product_id;
     }
     while(product_id < 0 || product_id > n);
-    
-    return 0;
-    
-    int binaryStep = log2(n);
-    printf("Worst case condition: we need %i steps or cycles\n", binaryStep);
+
+    printf("There are %li possible IDs\n", n);
 
     //iteration 1
-    long first = 0;
-    long last = n;
-    long mid = (first + last) / 2;
+    int iteration = 1;
+    int binaryStep = log2(n);
+    printf("Worst case condition: we need %i steps or cycles\n", binaryStep);
+    
+    for(iteration; iteration < 64; iteration++)
+    {
+        long first = iteration;
+        long last = n + 1;
+        long mid = (first + last) / 2;
+        if(product_id == mid)
+        {
+            printf("\n\tFound it!\n");
+            printf("Product ID found in Iteration %i\n", iteration);
+        }
+        else if(product_id < mid)
+        {
+            printf("Iteration 1:\n\tNeed to search in 1-50th percentile\n");
+        }
+        else
+        {
+            printf("Iteration 1:\n\tNeed to search in 51-100th percentile\n");
+        }
+            printf("This is last number at the end of cycle 1: %li\n", n / 2);
+    }
 
-    if(product_id == mid)
-    {
-        printf("Iteration 1:\n\tFound it!\n");
-    }
-    else if(product_id < mid)
-    {
-        printf("Iteration 1:\n\tNeed to search in 1-50th percentile\n");
-    }
-    else
-    {
-        printf("Iteration 1:\n\tNeed to search in 51-100th percentile\n");
-    }
-    printf("This is last number at the end of cycle 1: %li\n", n / 2);
 }
